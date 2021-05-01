a80765fdab03985c6888a3f78f7757d05008063d
Kelvin <kelvin0898@live.com>
2021-05-01 22:25:53 +1000

    Setting up some attributes for the bullet

--- src/settings.py
+++ src/settings.py
@@ -5,3 +5,8 @@ class Settings:
         # Initialize the game's settings
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
