cbcb5804406d773302420e0f59c5e5bf27bd99f8
Kelvin <kelvin0898@live.com>
2021-05-01 13:47:39 +1000

    Fixing mistake in getting the ship asset from images folder

--- src/ship.py
+++ src/ship.py
@@ -10,11 +10,10 @@ class Ship:
         self.screen_rect = ai_game.screen.get_rect()
 
         # Loading the ship and get its rect
-        self.image = pygame.image.load("images\ship.ship.bmp")
+        self.image = pygame.image.load("images\ship.bmp")
         self.rect = self.image.get_rect()
         # Start ech new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
-
 
     def blitme(self):
         # Draw  the ship at its current location
