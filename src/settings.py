ff9de2633f2417a029128bd17a6a6d2bab2d0fcd
Kelvin <kelvin0898@live.com>
2021-05-01 11:01:19 +1000

    Creating a settings module

---
+++ src/settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    # Class to store ll settings for the game
+
+    def __init__(self):
+        # Initialize the game's settings
+        # The first one is screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
