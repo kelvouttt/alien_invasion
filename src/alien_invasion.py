ef7d2e1f8d6c8cd44c8f397328464db10c4ea5a9
Kelvin <kelvin0898@live.com>
2021-05-01 10:27:58 +1000

    Initializing the game design

---
+++ src/alien_invasion.py
@@ -0,0 +1,35 @@
+import sys 
+
+import pygame 
+
+class AlienInvasion:
+    # class to manage game assets and behavior
+
+    def __init__(self):
+        # initializing the game, and create game resources
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        # starting the main loop for the game
+        while True:
+            # watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # make the most recently drawn screen visible
+            pygame.display.flip()
+
+    
+if __name__ == '__main__':
+    # make the game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
+
+        
+
+
+
