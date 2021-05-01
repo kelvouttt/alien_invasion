b54979b2c504487dabfff09981d2ebe957969d18
Kelvin <kelvin0898@live.com>
2021-05-01 11:00:36 +1000

    Importing settings module for screen size and background color

--- src/alien_invasion.py
+++ src/alien_invasion.py
@@ -1,36 +1,37 @@
 import sys 
 
-import pygame 
+import pygame
+from settings import Settings
 
 class AlienInvasion:
-    # class to manage game assets and behavior
+    # Class to manage game assets and behavior
 
     def __init__(self):
-        # initializing the game, and create game resources
+        # Initializing the game, and create game resources
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
-        self.bg_color = (230, 230, 230)
-
     def run_game(self):
-        # starting the main loop for the game
+        # Starting the main loop for the game
         while True:
             # watch for keyboard and mouse events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
 
-            # redraw the screen during each pass through the loop
-            self.screen.fill(self.bg_color)
+            # Redraw the screen during each pass through the loop
+            self.screen.fill(self.settings.bg_color)
 
-            # make the most recently drawn screen visible
+            # Make the most recently drawn screen visible
             pygame.display.flip()
 
     
 if __name__ == '__main__':
-    # make the game instance, and run the game.
+    # Make the game instance, and run the game.
     ai = AlienInvasion()
     ai.run_game()
 
