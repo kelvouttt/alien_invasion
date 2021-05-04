ea593d6116be2113da19d2e8385217e2eb608351
Kelvin <kelvin0898@live.com>
2021-05-04 17:49:33 +1000

    Setting for alien's ship

--- src/settings.py
+++ src/settings.py
@@ -4,10 +4,14 @@ class Settings:
     def __init__(self):
         # Initialize the game's settings
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1.5
+        self.ship_speed = 1.0
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 10
         self.bullet_height = 20
         self.bullet_color = (60, 60, 60)
         self.bullet_limit = 100
+        self.alien_speed = 0.7
+        self.fleet_drop_speed = 10
+        # fleet_direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
