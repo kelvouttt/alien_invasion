a08dbbae95dbf43fd47c70f0670922d8c0bb86c8
Kelvin <kelvin0898@live.com>
2021-05-04 20:44:48 +1000

    Setting alien speed and drop speed

--- src/settings.py
+++ src/settings.py
@@ -5,13 +5,21 @@ class Settings:
         # Initialize the game's settings
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.0
+         # Ships limit for players
+        self.ship_limit = 3
+        
         # Bullet settings
         self.bullet_speed = 1.0
         self.bullet_width = 10
         self.bullet_height = 20
         self.bullet_color = (60, 60, 60)
         self.bullet_limit = 100
-        self.alien_speed = 0.7
-        self.fleet_drop_speed = 10
-        # fleet_direction of 1 represents right; -1 represents left
+        
+        # Alien and fleet settings
+        self.alien_speed = 1
+        self.fleet_drop_speed = 1
+        
+        # Fleet_direction of 1 represents right; -1 represents left
         self.fleet_direction = 1
+        
+       
